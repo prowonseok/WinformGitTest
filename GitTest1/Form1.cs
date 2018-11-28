d5170b40d81cfff2b66041cbf87a5f594bb6b54e
Kouknam <dnrska718@gmail.com>
2018-11-28 11:19:39 +0900

    버튼 MBox 기능 추가

--- GitTest1/Form1.cs
+++ GitTest1/Form1.cs
@@ -26,5 +26,10 @@ namespace GitTest1
         {
             MessageBox.Show("Test");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
