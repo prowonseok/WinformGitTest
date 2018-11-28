b8b26ef58407156677aa9cc2a11965f43bdb38f0
Kouknam <dnrska718@gmail.com>
2018-11-28 11:53:12 +0900

    왼쪽 체크박스 mbox

--- GitTest1/Form1.Designer.cs
+++ GitTest1/Form1.Designer.cs
@@ -31,9 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-
             this.label1 = new System.Windows.Forms.Label();
-
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
@@ -58,9 +57,7 @@
             // 
             // button3
             // 
-
             this.button3.Location = new System.Drawing.Point(459, 188);
-
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
@@ -68,7 +65,6 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-
             // label1
             // 
             this.label1.AutoSize = true;
@@ -78,15 +74,24 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
-
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(26, 403);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.label1);
-
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -104,7 +109,7 @@
         private System.Windows.Forms.Button button3;
 
         private System.Windows.Forms.Label label1;
-
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
